bb0873f657ff166b4bd3401a0abfe3237b5e7787
Carlos <carlos.oliveirajr@hotmail.com>
2026-01-09 16:38:15 -0300

    feat: first commit. Project backend created

---
+++ backend/challenge/src/test/java/com/cso_jr/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cso_jr.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
